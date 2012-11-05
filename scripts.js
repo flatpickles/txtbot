d09bf6371c70e3865a4bca2fe73e7abb81f9e1bc
Matt Nichols <matt@mattnichols.net>
2012-11-05 16:17:58 -0500

    automatic updates, etc

--- scripts.js
+++ scripts.js
@@ -1,30 +1,50 @@
-/*
-// reference to Firebase root
-var root = new Firebase('https://gamma.firebase.com/overheard');
-
-// trigger update when an entry is added to Firebase tree
-root.on('child_added', function (snapshot) {
-	// remove loading element if present
-	$('#loading').remove();
-	
-	// add message element
-	var elID = snapshot.name();
-	var message = snapshot.child('body').val();
-	var time = Date(snapshot.child('time').val());
-	jQuery('<div/>', {
-		id: elID,
-		text: message
-	}).addClass('msg').hide().prependTo('#list').slideDown().fadeIn();
-});
-*/
+var most_recent = 0;
 
 $(document).ready(function() {
-	$.getJSON("http://mattnichols.net:6288/entries?jsoncallback=?", {
-		'n': 5
-	}, function(data) {
-		$.each(data.items, function(i,item) {
-			alert(item);
-		});
-	});
-	
+	initialize(15);
+	setInterval(check_for_new, 5000);
 });
+
+function check_for_new() {
+	$.getJSON("http://mattnichols.net:6288/entries?callback=?", {
+		'n': 10,
+		'after': most_recent
+	}, function(data) {
+		$('#loading').remove();
+		$.each(data, load_handler);
+	});
+}
+
+function initialize(n) {
+	$.getJSON("http://mattnichols.net:6288/entries?callback=?", {
+		'n': n
+	}, function(data) {
+		$('#loading').remove();
+		$.each(data, load_handler);
+	});
+};
+
+function load_handler(key, value) {
+	var e = jQuery('<div/>', {
+		id: key,
+		text: value['text']
+	}).addClass('msg');
+	insert_sorted(e, '.msg', '#list');
+	most_recent = Math.max(most_recent, parseInt(value['time']));
+};
+
+function insert_sorted(el, class_type, container) {
+	var all = $(class_type);
+	var curr = 0;
+	
+	while (curr < all.length && parseInt(el.attr('id')) < parseInt($(all[curr]).attr('id'))) {
+		curr++;
+	}
+	
+	if (all.length) {
+		if (all[curr]) el.insertBefore(all[curr]);
+		else el.insertAfter(all[all.length - 1]);
+	} else {
+		el.prependTo(container);
+	}
+};
