6d1ee92ddd0df8b9c8045f04b0a7f42b8bcdf8a0
Matt Nichols <matt@mattnichols.net>
2012-11-05 10:49:46 -0500

    organization and no more firebase

--- backend/txtbot_server.py
+++ backend/txtbot_server.py
@@ -2,17 +2,23 @@ from flask import Flask, request, redirect, g
 from threading import Thread
 import twilio.twiml, requests, json, time, sqlite3
 
+### GLOBAL INITIALIZATIONS ###
+
 DATABASE = "messages.db"
-FIREBASE = "https://gamma.firebase.com/overheard.json"
 app = Flask(__name__)
 
 blacklist = ["nichols"]
 min_length = 3
 
-def is_valid(s):
-  return all(ord(c) < 128 for c in s) \
-     and not any(w in s.lower() for w in blacklist) \
-     and len(s) >= min_length
+### REQUEST METHODS ###
+
+@app.before_request
+def before():
+  g.db = sqlite3.connect(DATABASE)
+
+@app.teardown_request
+def teardown(exception):
+  g.db.close()
 
 @app.route("/", methods=['GET', 'POST'])
 def handle_sms():
@@ -21,9 +27,8 @@ def handle_sms():
   if not txt:
     return "No data received!"
   reply = get_entry()
-  # create a thead to add it (so we don't timeout)
-  t = Thread(target=add_entry, args=(txt, request.values.get('From', None),))
-  t.start()
+  # add it
+  add_entry(txt, request.values.get('From', None))
   # form response
   resp = twilio.twiml.Response()
   if reply:
@@ -32,11 +37,9 @@ def handle_sms():
 
 @app.route("/count", methods=['GET', 'POST'])
 def serve_count():
-  db = sqlite3.connect(DATABASE)
-  cur = db.cursor()
+  cur = g.db.cursor()
   cur.execute("select count(*) from entries")
   count = str(cur.fetchone()[0])
-  db.close()
   return count
 
 @app.route("/entries", methods=['GET', 'POST'])
@@ -47,12 +50,10 @@ def serve_messages():
   upper_time_bound = int(request.values.get('before', '999999999999'))
 
   # get data
-  db = sqlite3.connect(DATABASE)
-  cur = db.cursor()
+  cur = g.db.cursor()
   data = {}
   cur.execute("select * from entries where time > ? and time < ? order by time desc limit ?", [lower_time_bound, upper_time_bound, to_get])
   vals = cur.fetchall()
-  db.close()
 
   # parse data
   for row in vals:
@@ -65,9 +66,15 @@ def serve_messages():
   # return data
   return json.dumps(data, sort_keys=True)
 
+### HELPER METHODS ###
+
+def is_valid(s):
+  return all(ord(c) < 128 for c in s) \
+     and not any(w in s.lower() for w in blacklist) \
+     and len(s) >= min_length
+
 def get_entry():
-  db = sqlite3.connect(DATABASE)
-  cur = db.cursor()
+  cur = g.db.cursor()
   # check if table has entries
   cur.execute("select case when exists (select * from entries limit 1) then 1 else 0 end")
   if not int(cur.fetchone()[0]):
@@ -75,28 +82,21 @@ def get_entry():
   # get a random entry
   cur.execute("select text from entries order by random() limit 1")
   r = str(cur.fetchone()[0])
-  db.close()
   return r
 
 def add_entry(entry, origin):
   timestamp = round(time.time())
-  # get DB in this thread context
-  db = sqlite3.connect(DATABASE)
-  cur = db.cursor()
+  cur = g.db.cursor()
   # if exists in sqlite db, exit
   cur.execute("select case when exists (select * from entries where text=? limit 1) then 1 else 0 end", [entry])
   if int(cur.fetchone()[0]) or not is_valid(entry):
     return
   # add to sqlite DB
-  db.execute('insert into entries (text, origin, time) values (?, ?, ?)',
+  g.db.execute('insert into entries (text, origin, time) values (?, ?, ?)',
                [entry, origin, timestamp])
-  db.commit()
-  # add to Firebase
-  payload = {'time': timestamp, 'origin': origin, 'body': entry}
-  requests.post(FIREBASE, data=json.dumps(payload))
+  g.db.commit()
 
-  # close DB
-  db.close()
+### MAIN ###
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=6288, debug=True)
